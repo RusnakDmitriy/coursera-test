cd6e5b92f48110c81c9aa4dd7e28124b7157f740
Dmitriy <rusnak179@gmail.com>
2017-05-28 15:14:42 +0300

    Add some functions

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -8,8 +8,21 @@
         $scope.listOfFood="";
         $scope.messageAfterChecking="";
         $scope.LunchCheck=function(){
-            var msg="";
+            var msg=defineTypeOfMsg($scope.listOfFood);
             $scope.messageAfterChecking=msg;
         };
-    })
+        
+        function defineTypeOfMsg(str){
+            if(!str)
+                return "Please enter data first";
+            var arr = str.split(',');
+            if(arr.length<4){
+                //console.log(arr.length);
+                return "Enjoy!";
+            }
+            else{
+                return "Too much!";
+            }
+        }
+    }
 })();
